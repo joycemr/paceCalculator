873c3410e609dce492256b1128928548b6b1e594
Michael Joyce <michael.joyce@illiniarts.com>
2020-03-30 20:59:39 -0500

    refined basic calculations

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const time = 20.0;
+const time = '21:00';
 const distInMeters = 5000;
 
 function timeInMinutes(timeInSeconds) {
     const min = Math.floor(timeInSeconds / 60);
     const seconds = timeInSeconds - (min * 60);
-    return min + ':' + seconds;
+    return min + ':' + Math.round(seconds,2);
 }
 
 function pace100m(time, distInMeters) {
 
+    const timeArray = time.split(':');
+    const minutes = timeArray[0];
+    const seconds = timeArray[1];
+    const timeInSeconds = (minutes * 60) + parseInt(seconds);
+
+    // Inputs
+    console.log('Race Distance in meters: ' + distInMeters);
+    console.log('Race time: ' + minutes + ':' + seconds);
+
     // How many seconds?
-    const timeInSeconds = time * 60;
-    console.log(timeInSeconds);
+    console.log('total time in seconds: ' + timeInSeconds);
 
     // Seconds per/meter
     const secondsPerMeter = timeInSeconds / distInMeters;
-    console.log(secondsPerMeter);
+    console.log('seconds per meter: ' + secondsPerMeter);
 
     // time for 100m
     const timeFor100 = secondsPerMeter * 100;
@@ -34,12 +42,13 @@ function pace100m(time, distInMeters) {
     // time for 800
     const timeFor800 =  secondsPerMeter * 800;
     console.log('time for 800M in seconds: ' + timeFor800);
+    // In minutes
+    console.log('time for 800M in minutes: ' + timeInMinutes(timeFor800));
 
     // time for 1KM
     const timeFor1000 = secondsPerMeter * 1000;
     console.log('time for 1000M in seconds: ' + timeFor1000);
     // In minutes
-    console.log('time for 1000M in minutes: ' + timeFor1000 / 60);
     console.log('time for 1000M in minutes: ' + timeInMinutes(timeFor1000));
 
 }
